cf2782d43a0508d46887ac28e6eb2c82b8981a36
kabrapratik28 <kabrapratik28@gmail.com>
2013-11-11 10:05:37 +0530

    Started building functions incomplete functions and steps

--- cr.py
+++ cr.py
@@ -1,1 +1,42 @@
-print "Hello Crawler ...:) "
+'''
+1. url get from page
+2. normalized them
+3. check for done before or not
+	if done => add anchor only
+        else => 
+
+'''
+def seed_reader():
+#read all seed normalized it and then put it into back queue 
+
+def sitenamenormalization():
+#lower case 
+#normalizes sitr name www.asd.com => http://asd.com
+
+
+def url_checker_dupli():
+#checks is url used first time or used before
+#return 0 or 1 accordingly
+
+
+
+def url_visited():
+#mark that url as visited 
+
+
+
+
+
+
+def url_giver():
+# returns one url that want to fetch 
+
+
+
+def back_queue_feeder():
+#fill back queue 
+
+
+
+
+
