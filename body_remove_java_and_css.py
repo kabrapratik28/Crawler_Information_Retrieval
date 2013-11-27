2b1e45c4c1a11986b20574ffc814980836df3705
kabrapratik28 <kabrapratik28@gmail.com>
2013-11-27 11:16:57 +0530

    Focused Crawler

--- body_remove_java_and_css.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-
-
-soupobject = BeautifulSoup(open('z.html'))
-        #soupnormalized = soupobject.prettify()
-        #print soupnormalized
-nouse = [xjs.extract() for xjs in soupobject.findAll(['script', 'style'])]
-print (soupobject.body.get_text()).encode('utf-8')
-        
